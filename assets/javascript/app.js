0c99352144e7605c39cbff9241b9be4210c8b450
Sarfulah Stewart <fu.stew@yahoo.com>
2019-07-10 15:11:45 -0400

    radio buttons added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,129 @@
-// Set counter for 30seconds
-var counter = 30;
-var intervalId;
+$(document).ready(function () {
+    // Set counter for 30seconds
+    var counter = 30;
+    var intervalId;
 
-$("#start").on("click", run);
-$("#stop").on("click", stop); 
 
-function run(){
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-}
-function decrement() {
-    counter--;
-    $("#show-number").html("<h2>" + counter + "</h2>");
-    if (counter === 0) {
-        stop();
-        alert("Times Up!");
+    $("#start").on("click", run);
+    $("#stop").on("click", stop);
+
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+        startGame();
+
     }
-}
+    function startGame() {
+        $("#start").hide();
+        for (var i = 0; i < questions.length; i++) {
+            // Display the question in a new paragraph
+            var questionP = $("<p>");
+            questionP.text(questions[i].question)
+            // Display each answer as a radio button
+            var optionA = $("<input type='radio'>");
+            optionA.text(questions[i].answers.a);
+            var optionB = $("<input type='radio'>");
+            optionB.text(questions[i].answers.b);
+            var optionC = $("<input type='radio'>");
+            optionC.text(questions[i].answers.c);
+            // Add a value for the answer to each radio button
+            // Append it to the page
+            $("#game").append(questionP);
+            $("#game").append("<label><input type='radio' name='" + i + "' value='" + questions[i].answers.a + "'>" + questions[i].answers.a + "</label>");
 
-// function startGame() {
-// // user clicks start and timer begins
-//     $("#start").hide();
-//     counter();
-//     question1();
-//     answerCheck();
-// })
-// }
-// var timer = new Timer();
-// timer.start();
-// timer.addEventListener('secondsUpdated', function (e) {
-//     $('#start').html(timer.getTimeValues().toString());
-// });
-// questions appear
+            $("#game").append(optionB);
+            $("#game").append(optionC);
 
-// user clicks answer, one chose 
+
+
+        }
+
+    }
+
+    function decrement() {
+        counter--;
+        $("#show-number").html("<h2>" + counter + "</h2>");
+        if (counter === 0) {
+            stop();
+            alert("Times Up!");
+        }
+
+    }
+    var questions = [
+
+
+
+
+
+
+        {
+            question: "What fast food restaurant is closed on Sundays?",
+            answers: {
+                a: "Chick-fil-A",
+                b: "Sonic",
+                c: "Checkers",
+            },
+            correctAnswer: "a"
+        },
+
+
+
+
+
+        {
+            question: "Which fast food restaurant was first to open in the U.S.?",
+            answers: {
+                a: "McDonalds",
+                b: "KFC",
+                c: "White Castle",
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What restaurant slogan is Finger Lickin Good?",
+            answers: {
+                a: "BurgerKing",
+                b: "Popeyes",
+                c: "KFC",
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What fastfood restaurant had a Chihuahua as their mascot?",
+            answers: {
+                a: "Chipotle",
+                b: "Taco Bell",
+                c: "Qdoba",
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "What fastfood restaurant has the Golden Arches?",
+            answers: {
+                a: "Sonic",
+                b: "McDonalds",
+                c: "Burger King",
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "Where did Burger King open their first franchise?",
+            answers: {
+                a: "Miami",
+                b: "Texas",
+                c: "Los Angeles",
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "What fastfood restaurant usually collabs with Taco Bell?",
+            answers: {
+                a: "Little Ceasers",
+                b: "Pizza Hut",
+                c: "Papa Johns",
+            },
+            correctAnswer: "b"
+        }
+    ]
+})
+
